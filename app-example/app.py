7222f5636aa5699d8348d5d64b6d1d7b6b3cc7c1
fortunewang <fortunewang@qq.com>
2019-07-31 12:55:55 +0800

    add example for rpc response

--- app-example/app.py
+++ app-example/app.py
@@ -1,5 +1,6 @@
 import json
 import pika
+from uuid import uuid4
 
 class App:
 
@@ -15,11 +16,21 @@ class App:
             queue=self.queue_name,
             on_message_callback=self.on_message,
             auto_ack=True)
+        res = self.channel.queue_declare(queue='', exclusive=True)
+        self.rpc_response_queue_name = res.method.queue
+        self.channel.basic_consume(
+            queue=self.rpc_response_queue_name,
+            on_message_callback=self.on_rpc_response,
+            auto_ack=True)
 
     def start(self):
         print('start_consuming')
         self.channel.start_consuming()
     
+    def on_rpc_response(self, channel, delivery, props, payload):
+        payload = json.loads(payload)
+        print(f'rpc response: {payload}')
+
     def on_message(self, channel, delivery, props, payload):
         qq, message_type = delivery.routing_key.split('.')
         qq = int(qq)
@@ -34,7 +45,10 @@ class App:
                     'to': message_from,
                     'message': message,
                 },
-            }))
+            }), properties=pika.BasicProperties(
+                reply_to=self.rpc_response_queue_name,
+                correlation_id=str(uuid4()),
+            ))
 
 if __name__ == '__main__':
     params = pika.ConnectionParameters(
