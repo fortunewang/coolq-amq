ba3ec14ad1e1bea032432b3539d7363be6a94fc2
fortunewang <fortunewang@qq.com>
2019-12-27 09:05:15 +0800

    use serde to parse configure file

--- cqamq/src/config.rs
+++ cqamq/src/config.rs
@@ -1,57 +1,91 @@
-use failure::{Fallible, err_msg};
-use lapin::uri::AMQPUri;
+use std::net::IpAddr;
+use serde::Deserialize;
+use failure::Fallible;
+use lapin::uri::{self as amqpuri, AMQPUri};
+
+#[inline]
+fn default_host() -> IpAddr { IpAddr::V4(std::net::Ipv4Addr::new(127, 0, 0, 1)) }
+
+#[inline]
+fn default_port() -> u16 { 5672 }
+
+#[inline]
+fn default_vhost() -> String { String::from("/") }
+
+#[inline]
+fn default_username() -> String { String::from("guest") }
+
+#[inline]
+fn default_password() -> String { String::from("guest") }
+
+#[inline]
+fn default_connection_timeout() -> u64 { 10 }
+
+#[derive(Deserialize)]
+struct Options {
+    #[serde(default = "default_host")]
+    pub host: IpAddr,
+    #[serde(default = "default_port")]
+    pub port: u16,
+    #[serde(default = "default_vhost")]
+    pub vhost: String,
+    #[serde(default = "default_username")]
+    pub username: String,
+    #[serde(default = "default_password")]
+    pub password: String,
+    #[serde(default = "default_connection_timeout")]
+    pub connection_timeout: u64,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            host: default_host(),
+            port: default_port(),
+            vhost: default_vhost(),
+            username: default_username(),
+            password: default_password(),
+            connection_timeout: default_connection_timeout(),
+        }
+    }
+}
 
 pub struct Config {
     pub uri: AMQPUri,
     pub connection_timeout: u64,
 }
 
+impl From<Options> for Config {
+    fn from(options: Options) -> Self {
+        let uri = AMQPUri {
+            scheme: amqpuri::AMQPScheme::AMQP,
+            authority: amqpuri::AMQPAuthority {
+                userinfo: amqpuri::AMQPUserInfo {
+                    username: options.username,
+                    password: options.password,
+                },
+                host: options.host.to_string(),
+                port: options.port,
+            },
+            vhost: options.vhost,
+            query: amqpuri::AMQPQueryString::default(),
+        };
+        let connection_timeout = options.connection_timeout;
+        Self { uri, connection_timeout }
+    }
+}
+
 pub fn read_config(app_dir: &str) -> Fallible<Config> {
     use std::path::Path;
-
-    let mut config = Config {
-        uri: AMQPUri::default(),
-        connection_timeout: 10,
-    };
 
     let config_path = Path::new(app_dir).join("config.toml");
 
     if !config_path.exists() {
-        return Ok(config);
+        return Ok(Config::from(Options::default()));
     }
 
     let options = std::fs::read_to_string(&config_path)?;
-    let options: toml::Value = toml::from_str(&options)?;
+    let options: Options = toml::from_str(&options)?;
 
-    if let Some(host) = options.get("host") {
-        config.uri.authority.host = host.as_str()
-            .ok_or(err_msg("config 'host' is not a string"))?
-            .to_owned();
-    }
-    if let Some(port) = options.get("port") {
-        config.uri.authority.port = port.as_integer()
-            .ok_or(err_msg("config 'host' is not an integer"))?
-            as u16;
-    }
-    if let Some(username) = options.get("username") {
-        config.uri.authority.userinfo.username = username.as_str()
-            .ok_or(err_msg("config 'username' is not a string"))?
-            .to_owned();
-    }
-    if let Some(password) = options.get("password") {
-        config.uri.authority.userinfo.password = password.as_str()
-            .ok_or(err_msg("config 'password' is not a string"))?
-            .to_owned();
-    }
-    if let Some(vhost) = options.get("vhost") {
-        config.uri.vhost = vhost.as_str()
-            .ok_or(err_msg("config 'vhost' is not a string"))?
-            .to_owned();
-    }
-    if let Some(connection_timeout) = options.get("connection_timeout") {
-        config.connection_timeout = connection_timeout.as_integer()
-            .ok_or(err_msg("config 'connection_timeout' is not an integer"))?
-            as u64;
-    }
-    return Ok(config);
+    return Ok(Config::from(options));
 }
